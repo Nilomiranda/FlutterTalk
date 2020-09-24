50a67c2a881a341556ef2b423274c4a582cccfe2
Nilomiranda <nilomiranda3@gmail.com>
2020-09-24 12:33:31 -0300

    applying validation to sign in form

--- lib/src/components/input.dart
+++ lib/src/components/input.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Input extends StatelessWidget {
-  Input({
-    @required this.placeholder,
-    this.type = TextInputType.text,
-  });
+  Input(
+      {@required this.placeholder,
+      this.type = TextInputType.text,
+      this.validator});
 
   final String placeholder;
   final TextInputType type;
+  final FormFieldValidator validator;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,9 @@ class Input extends StatelessWidget {
           decoration: InputDecoration(hintText: this.placeholder),
           obscureText:
               this.type == TextInputType.visiblePassword ? true : false,
-          keyboardType: this.type),
+          keyboardType: this.type,
+          validator: this.validator,
+          ),
       margin: EdgeInsets.only(bottom: 20.0),
     ));
   }
--- lib/src/pages/auth/signin.dart
+++ lib/src/pages/auth/signin.dart
@@ -1,9 +1,13 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:talk/src/components/button.dart';
 import 'package:talk/src/components/input.dart';
 
 class SignInPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return (MaterialApp(
@@ -14,6 +18,8 @@ class SignInPage extends StatelessWidget {
         child: ListView(
           children: [
             Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
                   child: Image(
@@ -24,41 +30,58 @@ class SignInPage extends StatelessWidget {
                     width: MediaQuery.of(context).size.width * 0.70,
                     margin: EdgeInsets.only(bottom: 15.0),
                     child: Form(
+                      key: _formKey,
                       child: Column(
                         children: [
                           Input(
                             placeholder: 'Your email',
                             type: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please, enter a valid email';
+                              }
+                              return null;
+                            },
                           ),
                           Input(
                               placeholder: 'Your desired password',
                               type: TextInputType.visiblePassword),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.70,
+                            margin: EdgeInsets.only(top: 10.0),
+                            child: Column(
+                              children: [
+                                Button(
+                                  label: 'Sign in',
+                                  onPressed: () {
+                                    if (_formKey.currentState.validate()) {
+                                      Navigator.pop(context);
+                                    } else {
+                                      return AlertDialog(
+                                        title: Text('Validation error'),
+                                        content: Center(child: Text('You probably missed some fields, check again...'),)
+                                      );
+                                    }
+                                  },
+                                  appearance: ButtonAppearance.solid,
+                                ),
+                                Button(
+                                  label: 'Create account',
+                                  onPressed: () => {true},
+                                  appearance: ButtonAppearance.outline,
+                                ),
+                                Container(
+                                  child: Text(
+                                    'I forgot my password 😢',
+                                  ),
+                                  margin: EdgeInsets.only(top: 10.0),
+                                )
+                              ],
+                            ),
+                          )
                         ],
                       ),
                     )),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.70,
-                  child: Column(
-                    children: [
-                      Button(
-                        label: 'Sign in',
-                        onPressed: () => {true},
-                        appearance: ButtonAppearance.solid,
-                      ),
-                      Button(
-                        label: 'Create account',
-                        onPressed: () => {true},
-                        appearance: ButtonAppearance.outline,
-                      ),
-                      Container(
-                        child: Text(
-                          'I forgot my password 😢',
-                        ),
-                        margin: EdgeInsets.only(top: 10.0),
-                      )
-                    ],
-                  ),
-                )
               ],
             )
           ],
