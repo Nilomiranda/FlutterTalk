1df355a36c6121cb629f600188c347a623ce2581
Nilomiranda <nilomiranda3@gmail.com>
2020-09-24 12:11:09 -0300

    adding scroll view to sign in screen

--- lib/src/components/input.dart
+++ lib/src/components/input.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Input extends StatelessWidget {
-  Input({@required this.placeholder});
-  
+  Input({
+    @required this.placeholder,
+    this.type = TextInputType.text,
+  });
+
   final String placeholder;
+  final TextInputType type;
 
   @override
   Widget build(BuildContext context) {
     return (Container(
       child: TextFormField(
-        decoration: InputDecoration(hintText: this.placeholder)
-      ),
+          decoration: InputDecoration(hintText: this.placeholder),
+          obscureText:
+              this.type == TextInputType.visiblePassword ? true : false,
+          keyboardType: this.type),
       margin: EdgeInsets.only(bottom: 20.0),
     ));
   }
--- lib/src/pages/auth/signin.dart
+++ lib/src/pages/auth/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:talk/src/components/button.dart';
 import 'package:talk/src/components/input.dart';
 
@@ -7,35 +8,60 @@ class SignInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return (MaterialApp(
         home: Scaffold(
+      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomPadding: true,
       body: Center(
-        child: (
-          Column(children: [
-            Container(
-              child: Image(image: AssetImage('assets/images/login.png'),),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.70,
-              margin: EdgeInsets.only(bottom: 15.0),
-              child: Form(
-                child: Column(children: [
-                  Input(placeholder: 'Your email'),
-                  Input(placeholder: 'Your desired password'),
-                  Input(placeholder: 'Confirm your desired password'),
-                ],),
-              )
-            ),
-            Container (
-              width: MediaQuery.of(context).size.width * 0.70,
-              child: Column(children: [
-                Button(label: 'Sign in', onPressed: () => { true }, appearance: ButtonAppearance.solid,),
-                Button(label: 'Create account', onPressed: () => { true }, appearance: ButtonAppearance.outline,),
+        child: ListView(
+          children: [
+            Column(
+              children: [
                 Container(
-                  child: Text('I forgot my password 😢',),
-                  margin: EdgeInsets.only(top: 10.0),
+                  child: Image(
+                    image: AssetImage('assets/images/login.png'),
+                  ),
+                ),
+                Container(
+                    width: MediaQuery.of(context).size.width * 0.70,
+                    margin: EdgeInsets.only(bottom: 15.0),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          Input(
+                            placeholder: 'Your email',
+                            type: TextInputType.emailAddress,
+                          ),
+                          Input(
+                              placeholder: 'Your desired password',
+                              type: TextInputType.visiblePassword),
+                        ],
+                      ),
+                    )),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.70,
+                  child: Column(
+                    children: [
+                      Button(
+                        label: 'Sign in',
+                        onPressed: () => {true},
+                        appearance: ButtonAppearance.solid,
+                      ),
+                      Button(
+                        label: 'Create account',
+                        onPressed: () => {true},
+                        appearance: ButtonAppearance.outline,
+                      ),
+                      Container(
+                        child: Text(
+                          'I forgot my password 😢',
+                        ),
+                        margin: EdgeInsets.only(top: 10.0),
+                      )
+                    ],
+                  ),
                 )
-              ],),
+              ],
             )
-          ], mainAxisSize: MainAxisSize.max, mainAxisAlignment: MainAxisAlignment.spaceAround,)
+          ],
         ),
       ),
     )));
