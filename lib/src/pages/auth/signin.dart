13a4af67c627a8c9f28ab5f452fffb61d6b2e8f3
Nilomiranda <nilomiranda3@gmail.com>
2020-09-24 10:58:26 -0300

    created sign in function

--- lib/src/pages/auth/signin.dart
+++ lib/src/pages/auth/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:talk/src/components/button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -6,7 +7,24 @@ class SignInPage extends StatelessWidget {
     return (MaterialApp(
         home: Scaffold(
       body: Center(
-        child: Text('Let us sign in'),
+        child: (
+          Column(children: [
+            Container(
+              child: Image(image: AssetImage('assets/images/login.png'),),
+            ),
+            Container (
+              width: MediaQuery.of(context).size.width * 0.70,
+              child: Column(children: [
+                Button(label: 'Sign in', onPressed: () => { true }, appearance: ButtonAppearance.solid,),
+                Button(label: 'Create account', onPressed: () => { true }, appearance: ButtonAppearance.outline,),
+                Container(
+                  child: Text('I forgot my password 😢',),
+                  margin: EdgeInsets.only(top: 10.0),
+                )
+              ],),
+            )
+          ], mainAxisSize: MainAxisSize.max, mainAxisAlignment: MainAxisAlignment.spaceAround,)
+        ),
       ),
     )));
   }
